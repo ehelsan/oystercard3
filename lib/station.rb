b9741be5fcd46980ee8d016825b5f0b75f0ec59d
ehelsan <ehelsan@hotmail.co.uk>
2021-06-15 09:51:36 +0100

    station

--- lib/station.rb
+++ lib/station.rb
@@ -1,16 +1,12 @@
 class Station
 
+  attr_reader :name
+  attr_reader :zone
+  
   def initialize(name, zone)
     @name = name
     @zone = zone 
   end
 
-  def name
-    @name
-  end
-
-  def zone
-    @zone
-  end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,13 @@
 require 'station'
 
 describe Station do
-  station = Station.new("Oxford Street", 3)
+  station = Station.new("Oxford Street", 1)
 
   it 'responds to initialize method - name' do
     expect(station.name).to eq "Oxford Street"
   end
 
   it 'responds to initialize method - zone' do
-    expect(station.zone).to eq 3
+    expect(station.zone).to eq 1
   end
 end
